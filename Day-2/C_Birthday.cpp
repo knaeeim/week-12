89ad665403f8ab5b733eb485c55b84662648b799
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2024-06-24 18:40:20 +0530

    added day-2

---
+++ Day-2/C_Birthday.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using order_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define vi vector<int>
+#define vll vector<ll>
+#define vp vector<pair<ll,ll>>
+#define sorta(arr) sort(arr.begin(), arr.end());
+#define sortd(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique((X).begin(),(X).end()),(X).end())
+#define range(arr) for(auto el: arr) cout<<el<<" ";
+
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+
+    int n; 
+    cin>>n; 
+
+    vi v(n); 
+
+    for(int i = 0; i < n; i++) cin>>v[i]; 
+
+    sortd(v);
+
+    vi ans; 
+
+    vi tem;
+
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0){
+            ans.pub(v[i]);
+        }
+        else{
+            tem.pub(v[i]);
+        }
+    }
+
+    sorta(tem); 
+
+    for(int i = 0; i < tem.size(); i++){
+        ans.pub(tem[i]);
+    }
+
+    range(ans);
+    cout<< endl;
+
+    return 0; 
+}
